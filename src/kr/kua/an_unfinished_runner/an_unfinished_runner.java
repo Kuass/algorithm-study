645e76f079319b1f3c78e78fbf461d90b4d895b5
Kua <kua@kakao.com>
2021-04-02 23:45:36 +0900

    완주하지 못한 선수

---
+++ src/kr/kua/an_unfinished_runner/an_unfinished_runner.java
@@ -0,0 +1,27 @@
+package kr.kua.an_unfinished_runner;
+
+import java.util.Arrays;
+
+public class an_unfinished_runner {
+    public an_unfinished_runner() {
+        String[] arr = new String[] {"leo", "kiki", "eden"};
+        String[] arr2 = new String[] {"eden", "kiki"};
+        String result = solution(arr, arr2);
+        System.out.println("result : " + result);
+    }
+
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i;
+        for(i = 0; i < completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+
+        return participant[i];
+    }
+}
+
---
+++ src/kr/kua/main.java
@@ -0,0 +1,9 @@
+package kr.kua;
+
+import kr.kua.an_unfinished_runner.an_unfinished_runner;
+
+public class main {
+    public static void main(String[] args) {
+        an_unfinished_runner aur = new an_unfinished_runner();
+    }
+}
