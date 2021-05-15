595c227286750831043bc07afdae5e2c1638c035
Kua <kua@kakao.com>
2021-05-15 22:30:08 +0900

    create baekjoon algorithm question

---
+++ src/kr/kua/baekjoon/question_1001/question_1001.java
@@ -0,0 +1,16 @@
+package kr.kua.baekjoon.question_1001;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class question_1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        System.out.println(a - b);
+    }
+}
+
