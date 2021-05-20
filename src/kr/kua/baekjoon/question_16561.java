49a9a98e81053ead33914ea29e1dbe3693e067c2
Kua <kua@kakao.com>
2021-05-20 20:41:02 +0900

    resolve the baekjoon question 11718, 16561

---
+++ src/kr/kua/baekjoon/question_16561.java
@@ -0,0 +1,21 @@
+package kr.kua.baekjoon;
+
+import java.util.Scanner;
+
+public class question_16561 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+
+        int sum = 1;
+        int count = 2;
+        for(int i = 9; i < a; i += 3) {
+            sum += count;
+            count += 1;
+        }
+
+        System.out.println(sum);
+    }
+}
+
