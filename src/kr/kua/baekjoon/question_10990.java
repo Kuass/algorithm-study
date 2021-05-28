2ae47910eabe3ace29d4b53666844dc3fbfef924
Kuass <kua@kakao.com>
2021-05-28 15:38:01 +0900

    resolve the baekjoon question 10992

--- src/kr/kua/baekjoon/question_10990.java
+++ src/kr/kua/baekjoon/question_10990.java
@@ -2,7 +2,7 @@ package kr.kua.baekjoon;
 
 import java.util.Scanner;
 
-public class question_10990 {
+public class question_10990 { // 별 찍기 - 15
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
