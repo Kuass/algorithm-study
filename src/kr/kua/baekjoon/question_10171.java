ef7156bb008d0c0d87302126cfc44bf12b14a1f0
Kuass <kua@kakao.com>
2021-06-16 13:51:39 +0900

    resolve the baekjoon question 10171, 10172

---
+++ src/kr/kua/baekjoon/question_10171.java
@@ -0,0 +1,10 @@
+package kr.kua.baekjoon;
+
+public class question_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
