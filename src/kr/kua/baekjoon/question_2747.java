bff9297ac9346eee87b696bcfd5f546ad9173b63
HoYoung <kua@kakao.com>
2021-07-16 15:16:22 +0900

    resolve the baekjoon question 2747

---
+++ src/kr/kua/baekjoon/question_2747.java
@@ -0,0 +1,21 @@
+package kr.kua.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class question_2747 { // 피보나치 수
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var n = Integer.parseInt(br.readLine());
+        var F = new int[] { 0, 1, 1 };
+
+        for (var i = 1; i < n; i++) {
+            F[2] = F[0] + F[1];
+            F[0] = F[1];
+            F[1] = F[2];
+        }
+
+        System.out.print(F[2]);
+    }
+}
