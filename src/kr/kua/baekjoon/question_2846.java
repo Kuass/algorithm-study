d47d3ae9a67d0d01c33c1e6284d0a8b66f7a6f48
HoYoung <kua@kakao.com>
2021-08-12 16:24:27 +0900

    resolve the baekjoon question 2846(오르막길)

---
+++ src/kr/kua/baekjoon/question_2846.java
@@ -0,0 +1,29 @@
+package kr.kua.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class question_2846 { // 오르막길
+    public static void main(String[] args) throws IOException, ParseException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        var H = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        var list = new ArrayList<Integer>();
+        var meter = 0;
+        for (int i = 0; i < H.length; i++)
+            if (meter == 0) {
+                if (H.length == i + 1) break;
+                else if (H[i] < H[i + 1]) meter = H[i + 1] - H[i];
+            } else if (H.length == i + 1 || H[i] >= H[i + 1]) {
+                list.add(meter);
+                meter = 0;
+            } else meter += H[i + 1] - H[i];
+
+        System.out.print(list.stream().mapToInt(x -> x).max().orElse(0));
+    }
+}
