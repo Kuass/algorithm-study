90a88015f22ce24c0ded9a5191f3f274acd9e0a4
HoYoung <kua@kakao.com>
2021-08-12 16:35:40 +0900

    resolve the baekjoon question 2857(FBI)

---
+++ src/kr/kua/baekjoon/question_2857.java
@@ -0,0 +1,22 @@
+package kr.kua.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+
+public class question_2857 { // FBI
+    public static void main(String[] args) throws IOException, ParseException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+
+        var sb = new StringBuilder();
+        var find = false;
+        for (var i = 0; i < 5; i++)
+            if (br.readLine().contains("FBI")) {
+                sb.append(i + 1).append(" ");
+                find = true;
+            }
+
+        System.out.print((find) ? sb.toString() : "HE GOT AWAY!");
+    }
+}
