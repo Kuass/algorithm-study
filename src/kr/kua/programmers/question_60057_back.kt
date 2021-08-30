2ff101a5ad7e99246e9442acf055b86b767b309c
HoYoung <kua@kakao.com>
2021-08-30 17:31:43 +0900

    resolve the programmers question 60057(문자열 압축)

--- src/kr/kua/programmers/question_60057_back.kt
+++ src/kr/kua/programmers/question_60057_back.kt
@@ -2,7 +2,7 @@ package kr.kua.programmers
 
 import java.util.*
 
-class question_60057 {
+class question_60057_back {
     fun question_60057() {
 //        println("aabbaccc | " + solution("aabbaccc"))
 //        println("ababcdcdababcdcd | " + solution("ababcdcdababcdcd"))
@@ -15,40 +15,30 @@ class question_60057 {
         println("a*1000 | " + solution("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
     }
 
-    fun solution(s: String): Int {
-        var level = 1
-        val size = s.length
-        var min = s.length
-
-        while (level <= size) {
-            var str = s.substring(0, level)
-            var isChange = false
-            val sb = StringBuilder()
-            var cnt = 2
-            var temp = ""
-
-            var j = level
-            while (j + level <= size) {
-                val sub = s.substring(j, j + level)
-                if (str == sub) {
-                    isChange = true
-                    temp = cnt.toString() + str
-                    cnt++
-                } else {
-                    sb.append(if(isChange) temp else str)
-                    isChange = false
-                    str = sub
-                    cnt = 2
-                    temp = ""
-
+    fun solution(s: String): Int { // case 2번 20번 에러!!! https://programmers.co.kr/questions/8103
+        val len = s.length
+        var min = Int.MAX_VALUE
+        for (i in 1..len) {
+            val stack = Stack<String>()
+            var count = 0
+            s.chunked(i).forEach {
+                if (stack.empty()) stack.push(it)
+                else {
+                    if (stack.peek() == it) count++
+                    else if (count != 0) {
+                        stack.push(count.toString())
+                        stack.push(it)
+                        count = 0
+                    } else stack.push(it)
                 }
-                j += level
             }
-            sb.append(if(temp.isEmpty()) s.substring(j-level, size) else temp + s.substring(j,size))
-            println(sb)
-            min = minOf(min, sb.length)
-            level++
+            if (count != 0) stack.push(count.toString())
+
+            var n = 0
+            while(stack.isNotEmpty()) n += stack.pop().length
+            if (min > n) min = n
         }
+
         return min
     }
 }
