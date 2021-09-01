3a718068187d893e6e14330d75ee22889710aafa
HoYoung <kua@kakao.com>
2021-09-01 22:30:29 +0900

    update

--- src/kr/kua/main.java
+++ src/kr/kua/main.java
@@ -1,9 +1,10 @@
 package kr.kua;
 
-import kr.kua.programmers.target_number;
+import kr.kua.programmers.question_42888;
 
 public class main {
     public static void main(String[] args) {
-        target_number rni = new target_number();
+        question_42888 rni = new question_42888();
+        rni.question_42888();
     }
 }
