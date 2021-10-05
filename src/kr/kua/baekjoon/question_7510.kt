b3c7b40a1d95ec2adc9c7e1cd3c08a4e78489e08
HoYoung <kua@kakao.com>
2021-10-05 09:49:17 +0900

    resolve the baekjoon question 7510(고급수학)

--- src/kr/kua/baekjoon/question_7510.kt
+++ src/kr/kua/baekjoon/question_7510.kt
@@ -3,9 +3,9 @@ package kr.kua.baekjoon
 import kotlin.math.pow
 
 fun main() = with(System.`in`.bufferedReader()) { // 고급 수학
-    repeat(readLine().toInt()) { it ->
-        println("Scenario #${it+1}")
-        val math = readLine().split(" ").map { i -> i.toDouble().pow(2) }
+    repeat(readLine().toInt()) {
+        println("Scenario #${it+1}:")
+        val math = readLine().split(" ").map { i -> i.toDouble().pow(2) }.sorted()
         println("${if(math[2] == math[0] + math[1]) "yes" else "no"}\n")
     }
 }
