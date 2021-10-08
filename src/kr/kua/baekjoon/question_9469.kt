bf7c31be7d1cffd6794f726dbb8ef4bb8312169c
HoYoung <kua@kakao.com>
2021-10-08 17:12:33 +0900

    resolve the baekjoon question 9469(폰 노이만)

---
+++ src/kr/kua/baekjoon/question_9469.kt
@@ -0,0 +1,8 @@
+package kr.kua.baekjoon
+
+fun main() = with(System.`in`.bufferedReader()) { // 폰 노이만
+    repeat(readLine().toInt()) {
+        println(readLine().split(" ").map { it.toDouble() }
+            .let { "${it[0].toInt()} ${java.text.DecimalFormat("#.000000").format(it[1] / (it[2] + it[3]) * it[4])}" })
+    }
+}
