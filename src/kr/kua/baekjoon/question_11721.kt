0e248dadbbb402acd3f328ea78e3586f22ab17bb
HoYoung <kua@kakao.com>
2021-10-09 11:55:45 +0900

    resolve the baekjoon question 11721(열 개식 끊어 출력하기)

---
+++ src/kr/kua/baekjoon/question_11721.kt
@@ -0,0 +1,8 @@
+package kr.kua.baekjoon
+
+fun main() = with(System.`in`.bufferedReader()) { // 열 개식 끊어 출력하기
+    var a = 0
+    val sb = StringBuilder()
+    readLine().toCharArray().forEach { if(++a % 10 == 0) sb.appendLine(it) else sb.append(it) }
+    print(sb)
+}
