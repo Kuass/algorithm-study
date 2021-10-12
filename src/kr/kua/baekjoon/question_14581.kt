df3763d70bbdeb00494a20e93d25bc509ceca446
HoYoung <kua@kakao.com>
2021-10-12 15:42:25 +0900

    resolve the baekjoon question 14581(팬들에게 둘러싸인 홍준)

---
+++ src/kr/kua/baekjoon/question_14581.kt
@@ -0,0 +1,7 @@
+package kr.kua.baekjoon
+
+fun main() = with(System.`in`.bufferedReader()) { // 팬들에게 둘러싸인 홍준
+    println(":fan::fan::fan:")
+    println(":fan::${readLine()}::fan:")
+    print(":fan::fan::fan:")
+}
