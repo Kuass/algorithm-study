0149cc0de76cd9b859ce7fd960d09450397c1db8
HoYoung <kua@kakao.com>
2021-10-15 09:56:30 +0900

    resolve the baekjoon question 16435(스네이크버드)

---
+++ src/kr/kua/baekjoon/question_16435.kt
@@ -0,0 +1,15 @@
+package kr.kua.baekjoon
+
+fun main() = with(System.`in`.bufferedReader()) { // 스네이크버드
+    var (N, L) = readLine().split(" ").map { it.toInt() }
+    val h = readLine().split(" ").map { it.toInt() }.toMutableList()
+    var i = -1
+    while(++i < h.size) {
+        if (h[i] <= L) {
+            h.removeAt(i)
+            L++
+            i = -1
+        }
+    }
+    print(L)
+}
