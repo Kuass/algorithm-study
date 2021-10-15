4c48a52b8f4f821e972c8f1563bc00a6f3b2f6e6
HoYoung <kua@kakao.com>
2021-10-15 18:30:28 +0900

    merge back solutions

--- src/kr/kua/programmers/question_42888.kt
+++ src/kr/kua/programmers/question_42888.kt
@@ -30,3 +30,60 @@ class question_42888 {
         return dialog
     }
 }
+
+class question_42888_back {
+    fun question_42888() {
+        val temp = arrayOf("Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan")
+        println("temp - ")
+        println(solution(temp))
+    }
+
+    fun solution(record: Array<String>): Array<String> {
+        val users = mutableListOf<User>()
+        val entrys = mutableListOf<Entry>()
+        record.forEach {
+            val info = it.split(" ")
+
+            var isValid = false
+            users.forEach {
+                if (it.id == info[1]) {
+                    isValid = true
+                    return@forEach
+                }
+            }
+
+            if (!isValid) users.add(User(info[1], info[2]))
+
+            when (info[0]) {
+                "Change" -> {
+                    var idx = -1
+                    users.forEachIndexed { index, it ->
+                        if (it.id == info[1]) {
+                            idx = index
+                            return@forEachIndexed
+                        }
+                    }
+                    users[idx].name = info[2]
+                }
+                else -> entrys.add(Entry(info[1], info[0] == "Enter"))
+            }
+        }
+
+        val result = Array(entrys.size) { "" }
+        entrys.forEachIndexed { index, it ->
+            var idx = -1
+            users.forEachIndexed { index2, it2 ->
+                if (it.id == it2.id) {
+                    idx = index2
+                    return@forEachIndexed
+                }
+            }
+            result[index] = users[idx].name + if(it.isEnter) "님이 들어왔습니다." else "님이 나갔습니다."
+        }
+
+        return result
+    }
+
+    data class User(val id: String, var name: String)
+    data class Entry(val id: String, val isEnter: Boolean)
+}
--- src/kr/kua/programmers/question_60057.kt
+++ src/kr/kua/programmers/question_60057.kt
@@ -52,3 +52,44 @@ class question_60057 {
         return min
     }
 }
+
+class question_60057_back {
+    fun question_60057() {
+//        println("aabbaccc | " + solution("aabbaccc"))
+//        println("ababcdcdababcdcd | " + solution("ababcdcdababcdcd"))
+//        println("abcabcdede | " + solution("abcabcdede"))
+//        println("abcabcabcabcdededededede | " + solution("abcabcabcabcdededededede"))
+//        println("xababcdcdababcdcd | " + solution("xababcdcdababcdcd"))
+        println("a*10 | " + solution("a".repeat(10)))
+        println("a*100 | " + solution("a".repeat(100)))
+        println("a*1000 | " + solution("a".repeat(1000)))
+        println("a*1000 | " + solution("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+    }
+
+    fun solution(s: String): Int { // case 2번 20번 에러!!! https://programmers.co.kr/questions/8103
+        val len = s.length
+        var min = Int.MAX_VALUE
+        for (i in 1..len) {
+            val stack = Stack<String>()
+            var count = 0
+            s.chunked(i).forEach {
+                if (stack.empty()) stack.push(it)
+                else {
+                    if (stack.peek() == it) count++
+                    else if (count != 0) {
+                        stack.push(count.toString())
+                        stack.push(it)
+                        count = 0
+                    } else stack.push(it)
+                }
+            }
+            if (count != 0) stack.push(count.toString())
+
+            var n = 0
+            while(stack.isNotEmpty()) n += stack.pop().length
+            if (min > n) min = n
+        }
+
+        return min
+    }
+}
