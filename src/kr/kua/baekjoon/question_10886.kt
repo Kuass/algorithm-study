1118ec4e102870c7d32dd497fbbd723dc17e1890
HoYoung <kua@kakao.com>
2021-10-25 17:35:00 +0900

    resolve the baekjoon question 10886(0 = not cute / 1 = cute)

---
+++ src/kr/kua/baekjoon/question_10886.kt
@@ -0,0 +1,12 @@
+package kr.kua.baekjoon
+
+fun main() = with(System.`in`.bufferedReader()) { // 0 = not cute / 1 = cute
+    var jh = 0
+    repeat(readLine().toInt()) {
+        when(readLine().toInt()) {
+            0 -> jh--
+            else -> jh++
+        }
+    }
+    print(if(jh > 0) "Junhee is cute!" else "Junhee is not cute!")
+}
