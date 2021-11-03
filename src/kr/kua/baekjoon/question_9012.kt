be344afd3930436b640e36bd71b95f06a6348427
HoYoung <kua@kakao.com>
2021-11-03 10:31:38 +0900

    resolve the baekjoon question 9012(괄호)

---
+++ src/kr/kua/baekjoon/question_9012.kt
@@ -0,0 +1,34 @@
+package kr.kua.baekjoon
+
+fun main() = with(System.`in`.bufferedReader()) { // 괄호
+    repeat(readLine().toInt()) {
+        val vps = readLine().toCharArray().toMutableList()
+        var bol = false
+        var i = 0
+        while(vps.isNotEmpty()) {
+            if (i >= vps.size) {
+                i = -1
+                break
+            } else if (bol) {
+                if (vps[i].code == 41) {
+                    bol = false
+                    vps.removeAt(i)
+                    i = 0
+                } else i++
+            } else {
+                if (vps[i].code != 40) {
+                    i = -1
+                    break
+                } else {
+                    bol = true
+                    vps.removeAt(i)
+                    if (vps.size == 0) {
+                        i = -1
+                        break
+                    }
+                }
+            }
+        }
+        println(if (i == -1) "NO" else "YES")
+    }
+}
