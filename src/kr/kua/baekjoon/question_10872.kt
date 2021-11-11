2b8bc1f7575ff271ea678841fb7686faa36af507
HoYoung <kua@kakao.com>
2021-11-11 17:21:24 +0900

    resolve the baekjoon question 10872(팩토리얼)

---
+++ src/kr/kua/baekjoon/question_10872.kt
@@ -0,0 +1,13 @@
+package kr.kua.baekjoon
+
+fun main() = with(System.`in`.bufferedReader()) { // 팩토리얼
+    print(Loop(readLine().toInt()))
+}
+
+fun Loop(N: Int): Int = if (N == 0) 1 else N * Loop(N - 1)
+
+fun main222() = with(System.`in`.bufferedReader()) { // 팩토리얼
+    var res = 1
+    repeat(readLine().toInt()) { res *= it + 1 }
+    print(res)
+}
