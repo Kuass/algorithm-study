6e5fdb1319b8e83b36d7aa989bcd012ffc8add36
HoYoung <kua@kakao.com>
2021-11-16 18:44:43 +0900

    inflearn course-푸샵맨.graph.numberOfIsland(섬의 수)

---
+++ src/kr/kua/inflearn/_푸샵맨/graph/numberOfIsland.java
@@ -0,0 +1,38 @@
+package kr.kua.inflearn._푸샵맨.graph;
+
+public class numberOfIsland {
+    public static void main(String args[]) {
+        char[][] grid = {
+                { '1', '1', '0', '0', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '1', '1', '1', '0', '0' },
+                { '0', '0', '0', '1', '1' }
+        };
+        System.out.print(solve(grid));
+    }
+
+    static int m, n, count;
+    static int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
+    public static int solve(char[][] grid) {
+        m = grid.length; // 4
+        n = grid[0].length; // 5
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1') return;
+        grid[i][j] = 'X';
+
+        for (int[] dir : dirs) dfs(grid, i + dir[0], j + dir[1]);
+    }
+}
