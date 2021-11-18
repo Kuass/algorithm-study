95a5b832b097c9fd85d13598e5ee28988493a99c
HoYoung <kua@kakao.com>
2021-11-18 18:26:14 +0900

    inflearn course-푸샵맨.strings.plusOne

---
+++ src/kr/kua/inflearn/_푸샵맨/strings/plusOne.java
@@ -0,0 +1,26 @@
+package kr.kua.inflearn._푸샵맨.strings;
+
+import java.util.Arrays;
+
+public class plusOne {
+    public static void main(String args[]) {
+        int[] temp = new int[] { 1,2,3 };
+        System.out.println(Arrays.toString(solve(temp)));
+    }
+
+    static int[] solve(int[] digits) {
+
+        int n = digits.length; // 3
+
+        for (int i = n-1; i >= 0; i--) {
+            digits[i]++;
+            if (digits[i] < 10) return digits;
+            digits[i] = 0;
+        }
+
+        int[] result = new int[n+1];
+        result[0] = 1;
+
+        return result;
+    }
+}
