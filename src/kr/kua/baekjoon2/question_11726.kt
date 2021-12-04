71ffe03e752d711b8a3459ce7faeaed7c91fcb3e
HoYoung <kua@kakao.com>
2021-12-04 18:03:07 +0900

    resolve the baekjoon 11726(2xn 타일링)

---
+++ src/kr/kua/baekjoon2/question_11726.kt
@@ -0,0 +1,11 @@
+package kr.kua.baekjoon2
+
+fun main() = with(System.`in`.bufferedReader()) { // 2xn 타일링
+    val N = readLine().toInt()
+    val dp = IntArray(N + 2)
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (i in 3..N) dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+    print(dp[N] % 10007)
+}
