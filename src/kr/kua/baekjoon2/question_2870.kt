ec9211afc971af376775a96bb9e10ed3586f4e4e
HoYoung <kua@kakao.com>
2022-01-04 10:10:04 +0900

    resolve the baekjoon question 2870(수학숙제)

---
+++ src/kr/kua/baekjoon2/question_2870.kt
@@ -0,0 +1,31 @@
+package kr.kua.baekjoon2
+
+import java.math.BigInteger
+
+fun main() = with(System.`in`.bufferedReader()) { // 수학숙제
+    val list = mutableListOf<BigInteger>()
+    repeat(readLine().toInt()) {
+        val str = readLine()
+
+        var startIndex = 0
+        var startDigit = false
+        for (i in 0..str.length) {
+            if (str.length == i) {
+                if (startDigit) list.add(str.substring(startIndex, i).toBigInteger())
+                break
+            }
+            if (str[i].isDigit()) {
+                if (!startDigit) startIndex = i
+                startDigit = true
+            } else  if (startDigit) {
+                startDigit = false
+                list.add(str.substring(startIndex, i).toBigInteger())
+            }
+        }
+    }
+
+    val sb = StringBuilder()
+    list.sort()
+    list.forEach { sb.appendLine(it) }
+    print(sb)
+}
