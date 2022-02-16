ae1167e9332098c69eb9c4c00d784c74643a36a8
HoYoung <kua@kakao.com>
2022-02-16 12:06:31 +0900

    resolve the programmers question 18258(큐 2)

---
+++ src/kr/kua/baekjoon2/question_18258.kt
@@ -0,0 +1,24 @@
+package kr.kua.baekjoon2
+
+import java.io.*
+import java.util.*
+
+// 왜틀리는지 모르겠음 (28%)
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) { // 큐 2
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val stack = ArrayDeque<Int>()
+    repeat(readLine().toInt()) {
+        val word = readLine()
+        if (word[1] == 'u') stack.push(word.split(' ')[1].toInt())
+        else bw.write(
+            when (word) {
+                "pop" -> if (stack.isEmpty()) -1 else stack.pop()
+                "size" -> stack.size
+                "front" -> if (stack.isEmpty()) -1 else stack.last
+                "back" -> if (stack.isEmpty()) -1 else stack.first
+                else -> if (stack.isEmpty()) 1 else 0
+            }.toString() + "\n"
+        )
+    }
+    bw.flush()
+}
